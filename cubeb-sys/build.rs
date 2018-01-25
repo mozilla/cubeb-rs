23f6c0fd21e795f217e4d2c234d7487109c27c75
Dan Glastonbury <dan.glastonbury@gmail.com>
2018-01-25 11:47:09 +1000

    Organise all the FFI stuff into cubeb-sys crate.

--- cubeb-sys/build.rs
+++ cubeb-sys/build.rs
@@ -1,3 +1,8 @@
+// Copyright © 2018 Mozilla Foundation
+//
+// This program is made available under an ISC-style license.  See the
+// accompanying file LICENSE for details.
+
 extern crate cmake;
 extern crate gcc;
 extern crate pkg_config;

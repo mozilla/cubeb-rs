c777d6e23bdc00267fdd93d48f5a59d08e4c5001
Matthew Gregan <kinetik@flim.org>
2021-07-23 16:42:14 +1200

    cubeb-core: Ignore clippy::self_named_constructor on `Error::error`.

--- cubeb-core/src/error.rs
+++ cubeb-core/src/error.rs
@@ -26,6 +26,10 @@ pub struct Error {
 }
 
 impl Error {
+    // `clippy::self_named_constructor` is a nightly-only lint as of 2021-07-22,
+    // so we allow `unknown_lints` to ignore it on stable.
+    #[allow(unknown_lints)]
+    #[allow(clippy::self_named_constructor)]
     pub fn error() -> Self {
         Error {
             code: ErrorCode::Error,

f7b50423025a7e4227e8759b4b3291b2e1c956d0
Matthew Gregan <kinetik@flim.org>
2025-09-17 16:47:47 +1200

    Fix formatting.

--- cubeb-core/build.rs
+++ cubeb-core/build.rs
@@ -3,10 +3,12 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let versioned = format!("rust_write_formatted_msg_{}_{}_{}",
-                            env!("CARGO_PKG_VERSION_MAJOR"),
-                            env!("CARGO_PKG_VERSION_MINOR"),
-                            env!("CARGO_PKG_VERSION_PATCH"));
+    let versioned = format!(
+        "rust_write_formatted_msg_{}_{}_{}",
+        env!("CARGO_PKG_VERSION_MAJOR"),
+        env!("CARGO_PKG_VERSION_MINOR"),
+        env!("CARGO_PKG_VERSION_PATCH")
+    );
 
     let mut cfg = cc::Build::new();
     cfg.file("src/log.c");
@@ -17,15 +19,18 @@ fn main() {
     let dest_path = Path::new(&out_dir).join("log_wrap.rs");
     fs::write(
         &dest_path,
-        format!(r#"
+        format!(
+            r#"
             #[no_mangle]
             pub unsafe extern "C" fn {}(s: *const c_char) {{
                 rust_write_formatted_msg(s);
             }}
-        "#, versioned)
-    ).unwrap();
+        "#,
+            versioned
+        ),
+    )
+    .unwrap();
 
     println!("cargo::rerun-if-changed=src/log.c");
     println!("cargo::rerun-if-changed=build.rs");
 }
-
